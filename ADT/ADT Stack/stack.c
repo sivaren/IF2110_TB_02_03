ac3bb2ffe81733371cc55b76cc390b571ae42e65
lyorafelicya <16520164@std.stei.itb.ac.id>
2021-10-29 21:10:00 +0700

    rename fungsi stack

--- ADT/ADT Stack/stack.c
+++ ADT/ADT Stack/stack.c
@@ -12,19 +12,19 @@ void CreateStack(Stack *s) {
 }
 
 /* ************ Prototype ************ */
-boolean isEmpty(Stack s) {
+boolean isEmptyStack(Stack s) {
 /* Mengirim true jika s kosong: lihat definisi di atas */
     /* ALGORITMA */
     return IDX_TOP(s) == IDX_UNDEF;
 }
-boolean isFull(Stack s) {
+boolean isFullStack(Stack s) {
 /* Mengirim true jika tabel penampung nilai s stack penuh */
     /* ALGORITMA */
-    return IDX_TOP(s) == CAPACITY-1;
+    return IDX_TOP(s) == CAPACITY_STACK-1;
 }
 
 /* ************ Menambahkan sebuah elemen ke Stack ************ */
-void push(Stack *s, ElType val) {
+void push(Stack *s, ElTypeStack val) {
 /* Menambahkan val sebagai elemen Stack s */
 /* I.S. s mungkin kosong, tabel penampung elemen stack TIDAK penuh */
 /* F.S. val menjadi TOP yang baru,IDX_TOP bertambah 1 */
@@ -34,7 +34,7 @@ void push(Stack *s, ElType val) {
 }
 
 /* ************ Menghapus sebuah elemen Stack ************ */
-void pop(Stack *s, ElType *val) {
+void pop(Stack *s, ElTypeStack *val) {
 /* Menghapus val dari Stack s */
 /* I.S. s tidak mungkin kosong */
 /* F.S. val adalah nilai elemen TOP yang lama, IDX_TOP berkurang 1 */
--- ADT/ADT Stack/stack.h
+++ ADT/ADT Stack/stack.h
@@ -8,11 +8,11 @@
 #include "boolean.h"
 
 #define IDX_UNDEF -1
-#define CAPACITY 100
+#define CAPACITY_STACK 100
 
-typedef int ElType;
+typedef int ElTypeStack;
 typedef struct {
-  ElType buffer[CAPACITY]; /* tabel penyimpan elemen */
+  ElTypeStack buffer[CAPACITY_STACK]; /* tabel penyimpan elemen */
   int idxTop;              /* alamat TOP: elemen puncak */
 } Stack;
 
@@ -29,19 +29,19 @@ void CreateStack(Stack *s);
 /* Proses : Melakukan alokasi, membuat sebuah s kosong */
 
 /* ************ Prototype ************ */
-boolean isEmpty(Stack s);
+boolean isEmptyStack(Stack s);
 /* Mengirim true jika s kosong: lihat definisi di atas */
-boolean isFull(Stack s);
+boolean isFullStack(Stack s);
 /* Mengirim true jika tabel penampung nilai s stack penuh */
 
 /* ************ Menambahkan sebuah elemen ke Stack ************ */
-void push(Stack *s, ElType val);
+void push(Stack *s, ElTypeStack val);
 /* Menambahkan val sebagai elemen Stack s */
 /* I.S. s mungkin kosong, tabel penampung elemen stack TIDAK penuh */
 /* F.S. val menjadi TOP yang baru,IDX_TOP bertambah 1 */
 
 /* ************ Menghapus sebuah elemen Stack ************ */
-void pop(Stack *s, ElType *val);
+void pop(Stack *s, ElTypeStack *val);
 /* Menghapus val dari Stack s */
 /* I.S. s tidak mungkin kosong */
 /* F.S. val adalah nilai elemen TOP yang lama, IDX_TOP berkurang 1 */
