6edd59347620e3dcc7a48c2ad4f1f470b037041d
ravaattar <rava.naufal2014@gmail.com>
2021-11-11 01:39:19 +0700

    added help and toDo

---
+++ help/help.h
@@ -0,0 +1,24 @@
+/* File: help.h */
+
+#ifndef HELP_H
+#define HELP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* NOTE:
+Masih nunggu adjust posisi jika lagi di HQ
+ */
+
+/* Display Help */
+void displayHelp();
+/*  Menampilkan seluruh command pada permainan
+    beserta deskripsi dari command tersebut. */
+/*  ENTER COMMAND: HELP
+    1. MOVE -> Untuk berpindah ke lokasi selanjutnya
+    2. PICK_UP -> Untuk mengambil item di lokasi terkini
+    …
+    8. INVENTORY -> Untuk melihat gadget yang dimiliki dan menggunakannya
+    9. HELP -> Untuk mengeluarkan list command dan kegunaannya  */
+    
+#endif
---
+++ toDo/toDo.h
@@ -0,0 +1,76 @@
+/* File: toDo.h */
+
+#ifndef TODO_h
+#define TODO_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "boolean.h"
+
+typedef struct {
+    int timeIn;
+    char pickUp;
+    char dropOff;
+    char itemType;
+    int perishTime;
+} ToDoType;
+
+/* 
+diganti kalo udah fix
+typedef pesanan ToDoType;
+ */
+
+typedef struct node *Address;
+typedef struct node { 
+	ToDoType info;
+	Address next;
+} ElmtToDoList;
+
+typedef Address ToDoList;
+
+#define PERISH_TIME_UNDEF  (-1)
+#define FIRST_TODO(td)  (td)
+#define INFO_TODO(td)   (td)->info
+#define NEXT_TODO(td)   (td)->next
+
+#define TIMEIN_TODO(td)   (td)->info.timeIn
+#define PICKUP_TODO(td)   (td)->info.pickUp
+#define DROPOFF_TODO(td)   (td)->info.dropOff
+#define ITEMTYPE_TODO(td)   (td)->info.itemType
+#define PERISHTIME_TODO(td)   (td)->info.perishTime
+
+/* 
+TIMEIN_TODO(td)
+PICKUP_TODO(td)
+DROPOFF_TODO(td)   
+ITEMTYPE_TODO(td)  
+PERISHTIME_TODO(td)
+*/
+
+/* alokasi new node untuk TO DO LIST */
+Address newToDoNode(int timeIn,char pickUp, char dropOff, 
+    char itemType, int perishTime);
+
+void CreateToDoList(ToDoList *td);
+/* I.S. sembarang             */
+/* F.S. Terbentuk list kosong */
+
+boolean isEmpty_ToDo(ToDoList td);
+/* Mengirim true jika list kosong */
+
+int length_ToDoList(ToDoList td);
+/* Mengirim banyaknya elemen list jika ada, jika tidak mengirimkan nol */
+
+void insert_ToDoList(ToDoList *td, ToDoType val);
+/* I.S. l mungkin kosong */
+/* F.S. Melakukan alokasi sebuah elemen dan */
+/* menambahkan elemen list di akhir: elemen terakhir yang baru */
+/* bernilai val jika alokasi berhasil. Jika alokasi gagal: I.S.= F.S. */
+
+void delete_ToDoList(ToDoList *td, ToDoType val, ToDoType *delVal);
+/* Menghapus elemen toDoList tertentu */
+
+void displayToDoList(ToDoList td);
+/* Menampilkan isi To Do List */
+
+#endif
