995ab1443ebf8e5c45b242ddd884e7987fab5b11
ziyaddr <16520263@mahasiswa.itb.ac.id>
2021-11-11 19:53:49 +0700

    next

--- PICK_UP/PICK_UP.h
+++ PICK_UP/PICK_UP.h
@@ -6,17 +6,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-// include
-// Daftar pesanan
-// Todo List
-// In Progress list & Tas
-// Daftar bangunan
-#include "../ADT/ADT Matriks/matrix.h"
-#include "../ADT/ADT Point/point.h"
-#include "../ADT/ADT List Statis/listpos.h"
-#include "../ADT/ADT Linked List/list_linked.h"
-#include "../ADT/ADT Queue/queue.h" // modif prioqueue
-#include "../ADT/ADT Stack/stack.h"
 #include "../ADT/ADT List Statis/ListOfBangunan.h"
 #include "../toDo/toDo.h"
 #include "../tas/tas.h"
---
+++ PICK_UP/mPICK_UP.c
@@ -0,0 +1,69 @@
+#include "PICK_UP.h"
+
+
+void readTas(Tas *Tasnobita, InProgList *Inprog, int *heavyitems) {
+    int capacity;
+    int jumlahPesanan;
+    boolean speedboost;
+    ElTypeTas tempElmt;
+    InProgType tempElmt2;
+    char tempItemType;
+    char tempPickUp;
+    char tempDropOff;
+    int tempPerishTime;
+    printf("masukkan capacity Tas: ");
+        scanf("%d", &capacity);
+        // create tas
+        CreateTas(Tasnobita, capacity);
+    printf("masukkan jumlah pesanan saat ini: ");
+        scanf("%d", &jumlahPesanan);
+        // iterasi masukkan elem ke tas
+        for (int i=jumlahPesanan; i>0; i--) {
+            printf("Tas elemen ke-%d\n", i);
+            printf("Masukkan item type: ");
+            scanf("%c", &tempItemType);
+            if (tempItemType == 'H') {
+                *heavyitems += 1;
+            }
+            printf("Masukkan pick up: ");
+            scanf("%c", &tempPickUp);
+            printf("Masukkan drop off: ");
+            scanf("%c", &tempDropOff);
+            printf("Masukkan tempPerishTme: ");
+            scanf("%d", &tempPerishTime);
+            tempElmt.itemType = tempItemType;
+            tempElmt.pickUp = tempPickUp;
+            tempElmt.dropOff = tempDropOff;
+            tempElmt.perishTime = tempPerishTime;
+            tempElmt2.itemType = tempItemType;
+            tempElmt2.pickUp = tempPickUp;
+            tempElmt2.dropOff = tempDropOff;
+            tempElmt2.perishTime = tempPerishTime;
+            pushTas(Tasnobita, tempElmt);
+            insertFirst_InProgList(Inprog, tempElmt2);
+        }
+    printf("speedboost (0 = false, 1 = true): ");
+        scanf("%d", &speedboost);
+}
+
+int main() {
+    // inisialisasi
+    ToDoList Todo;
+    InProgList DaftarInprog;
+    int heavyitems;
+    boolean speedboost;
+    Bangunan CurrentBangunan;
+        // baca input current bangunan
+        scanf("%c", &CurrentBangunan);
+    Tas TasNobita;
+        // baca input tas nobita
+        readTas(&TasNobita, &DaftarInprog, &heavyitems);
+    
+    displaytas(TasNobita);
+    displayInProg(DaftarInprog);
+
+
+
+
+    return 0;
+}
